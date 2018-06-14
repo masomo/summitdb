82ea63d2a49d5de0d876257d64611a625c40bc6a
semihalev <semihalev@gmail.com>
2018-06-15 00:08:08 +0300

    changed raftstore threshold

--- vendor/github.com/tidwall/raft-fastlog/store.go
+++ vendor/github.com/tidwall/raft-fastlog/store.go
@@ -27,7 +27,7 @@ var ErrKeyNotFound = errors.New("not found")
 var ErrClosed = errors.New("closed")
 var ErrShrinking = errors.New("shrink in progress")
 
-const minShrinkSize = 8 * 1024 * 1024
+const minShrinkSize = 64 * 1024 * 1024
 
 const (
 	cmdSet         = '(' // Key+Val
